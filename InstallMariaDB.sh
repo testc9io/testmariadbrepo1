bdf16f909b02cc7b96c37cf4108fb15640573364
testc9io <testc9io@yandex.ru>
2016-04-29 11:51:50 +0000

    auto commit

--- InstallMariaDB.sh
+++ InstallMariaDB.sh
@@ -2,7 +2,7 @@ sudo apt-get install software-properties-common
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 sudo add-apt-repository 'deb [arch=amd64,i386] http://mirror.mephi.ru/mariadb/repo/10.1/ubuntu trusty main'
 sudo apt-get update
-sudo apt-get install mariadb-server
+sudo apt-get install -y mariadb-server
 
 sudo pip3 install pymysql
 
